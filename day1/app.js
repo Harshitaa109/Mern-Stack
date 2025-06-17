da6c5c0db09cb5dfc1d1793509b8e097268ab7b0
HarshitaCoder <harshitasingh70803@gmail.com>
2025-06-17 10:30:50 +0530

    `Added detailed explanations and examples for variable declaration and initialization, type checking, hoisting, block scope, arrow functions, callbacks, function declarations, and function expressions.`

---
+++ day1/app.js
@@ -0,0 +1,8 @@
+window.console.log("Hello from JS!");
+
+//let age=22;
+//const year=2025;
+//var block=22;
+//console.log(age, year, block);
+var age=21;// declaration & initialization
+console.log(age);
---
+++ day2/app.js
@@ -0,0 +1,43 @@
+
+
+
+
+// _____________________________
+//4. arrow function assignment
+
+const printBill = (txt) => {
+    console.log("____________________");
+    console.log("Rs.",txt);
+    console.log("____________________");
+};
+/*const calculateBillAmountForFood = (price) => {
+    return price*1.05;
+};
+const calculateBillAmountForClothes = (price) => {
+    return price*1.12;
+};
+const calculateBillAmountForDrinks = (price) => {
+    return price*1.2;
+};
+
+const calculateTotal = (food, clothes, drinks) => {
+    const foodAmount= calculateBillAmountForFood(food);
+    const clothesAmount = calculateBillAmountForClothes(clothes);
+    const drinksAmount = calculateBillAmountForDrinks(drinks);
+    const finalAmount = foodAmount + clothesAmount + drinksAmount;
+    printBill(finalAmount);
+
+
+};*/
+
+const calculateBillAmountForFood = (price) => {
+   
+};
+const calculateBillAmountForClothes = (price) => {
+};
+const calculateBillAmountForDrinks = (price) => {
+};
+const generateBill = (food, clothes, drinks) => {
+
+};
+generateBill(100,400,100);
